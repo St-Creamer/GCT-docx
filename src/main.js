82871b25e2869df63217139a41dad0a876a6362d
St-Creamer <zakaria.hajlaoui@gmail.com>
2020-12-13 17:57:25 +0100

    prettier

--- src/main.js
+++ src/main.js
@@ -1,38 +1,37 @@
-const { app, BrowserWindow } = require('electron');
-const { dbAuth } = require('./db.js');
-const db = require('./db.js');
-const {ipcMain} = require ('electron')
+const { app, BrowserWindow } = require("electron");
+const { dbAuth } = require("./db.js");
+const db = require("./db.js");
+const { ipcMain } = require("electron");
 
-ipcMain.handle('create-item',(event,args)=>{
-  db.Item.sync()
-  db.Item.create({Amount :args.Amount , Disc : args.Disc})
-    .then(data =>{
-      console.log(data);
-    })
+ipcMain.handle("create-item", (event, args) => {
+  db.Item.sync();
+  db.Item.create({ Amount: args.Amount, Disc: args.Disc }).then((data) => {
+    console.log(data);
+  });
 });
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
-  })
+      nodeIntegration: true,
+    },
+  });
 
-  win.loadFile('src/index.html')
+  win.loadFile("src/index.html");
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
+});
